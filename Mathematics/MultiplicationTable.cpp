e495071ef4fd4cb31d7e03a643be144c0e7f1991
Mukul Madaan <mukul.madaan8@gmail.com>
2024-03-21 08:49:08 +0530

    Create MultiplicationTable.cpp

---
+++ Mathematics/MultiplicationTable.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+public:
+    vector<int> getTable(int N)
+    {
+        vector<int> result;
+        for(int i = 1; i <= 10; i++){
+            result.push_back(N * i);
+        }
+        return result;
+    }
+};
